f4221443b0d517f6717f539d296aaaf5cce9ed58
Patryk Jędrzejczak <patryk.jed123@gmail.com>
2023-01-15 18:36:23 +0100

    Add casting API to the C++ side of the clocks module
    This commit adds methods necessary to cast durations and type_points to and
    from `std::chrono::nanoseconds` after passing them across ffi as `int64_t`.
    
    C++ side of the timer module needs those methods. They are implemented in
    `clocks.{hh, cc}` because this is a more general place for them (they might
    be used in other place than the timer module).
    
    Some definicions introduced in this commit enabled simplification of already
    existent code in `timer.cc`. This is also done in this commit.

--- seastar/src/clocks.cc
+++ seastar/src/clocks.cc
@@ -7,34 +7,55 @@ using sc = seastar::steady_clock_type;
 using lc = seastar::lowres_clock;
 using mc = seastar::manual_clock;
 
-using nanos = std::chrono::nanoseconds;
-
 using std::chrono::duration_cast;
 
 int64_t steady_clock_now() {
     static_assert(std::is_same<int64_t, sc::rep>::value);
 
     sc::duration d = sc::now().time_since_epoch();
-    return duration_cast<nanos>(d).count();
+    return to_nanos(d).count();
 }
 
 int64_t lowres_clock_now() {
     static_assert(std::is_same<int64_t, lc::rep>::value);
 
     lc::duration d = lc::now().time_since_epoch();
-    return duration_cast<nanos>(d).count();
+    return to_nanos(d).count();
 }
 
 int64_t manual_clock_now() {
     static_assert(std::is_same<int64_t, mc::rep>::value);
 
     mc::duration d = mc::now().time_since_epoch();
-    return duration_cast<nanos>(d).count();
+    return to_nanos(d).count();
 }
 
 void manual_clock_advance(int64_t duration) {
-    nanos d(duration);
-    mc::advance(duration_cast<mc::duration>(d));
+    mc::advance(to_mc_duration(duration));
+}
+
+sc::duration to_sc_duration(int64_t duration) {
+    return duration_cast<sc::duration>(nanos(duration));
+}
+
+sc::time_point to_sc_time_point(int64_t tp) {
+    return sc::time_point(to_sc_duration(tp));
+}
+
+lc::duration to_lc_duration(int64_t duration) {
+    return duration_cast<lc::duration>(nanos(duration));
+}
+
+lc::time_point to_lc_time_point(int64_t tp) {
+    return lc::time_point(to_lc_duration(tp));
+}
+
+mc::duration to_mc_duration(int64_t duration) {
+    return duration_cast<mc::duration>(nanos(duration));
+}
+
+mc::time_point to_mc_time_point(int64_t tp) {
+    return mc::time_point(to_mc_duration(tp));
 }
 
 } // namespace clocks
