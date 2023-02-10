a417f20da699fd199515897ea22f39c6f2e57017
kfernandez31 <kacper.fernandez@gmail.com>
2023-02-10 21:22:08 +0100

    Fix test macro, make it spawn a thread per test

--- seastar-macros/src/lib.rs
+++ seastar-macros/src/lib.rs
@@ -4,13 +4,23 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, ItemFn};
 
-/// Procedural macro which provides easy way to write
-/// tests in Rust that run on Seastar runtime.
+/// A macro intended for running asynchronous tests.
+/// Tests are spawned in a separate thread.
+/// This is done to ensure thread_local cleanup between them
+/// (at the time of writing, Seastar doesn't do it itself).
+///
+/// # Usage
+///
+/// ```rust
+/// #[seastar_macros::test]
+/// async fn my_test() {
+///     assert!(true);
+/// }
+/// ```
 #[proc_macro_attribute]
 pub fn test(_args: TokenStream, item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as ItemFn);
 
-    let ret = &input.sig.output;
     let name = &input.sig.ident;
     let body = &input.block;
     let attrs = &input.attrs;
@@ -25,9 +35,19 @@ pub fn test(_args: TokenStream, item: TokenStream) -> TokenStream {
     let output = quote! {
         #[test]
         #(#attrs)*
-        fn #name() #ret {
-            let app = AppTemplate::new_from_options(Options::new());
-            app.run_void(&vec![String::from("test")], || { block_on(async { #body }); });
+        fn #name() {
+            std::thread::spawn(|| {
+                let _guard = crate::acquire_guard_for_seastar_test();
+                let mut app = AppTemplate::default();
+                let args = std::env::args().collect::<Vec<_>>();
+                let fut = async {
+                    #body
+                    Ok(())
+                };
+                app.run_void(&args[..], fut);
+            })
+            .join()
+            .unwrap();
         }
     };
 
