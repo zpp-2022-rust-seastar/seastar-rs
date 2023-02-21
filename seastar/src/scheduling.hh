fec3bdca0289ca34a2d09880eb58e7254f764714
Patryk Jędrzejczak <patryk.jed123@gmail.com>
2023-02-21 13:36:54 +0100

    Add SchedulingGroup and implement the Default trait for it
    This commit introduces the `SchedulingGroup` type, which is an equivalent of
    `seastar::scheduling_group`.
    
    It also implements the Default trait for `SchedulingGroup` which corresponds
    to the parameterless constructor of the `scheduling_group` class (and
    funtion `default_scheduling_group` that is just a wrapper on the contructor).

--- seastar/src/scheduling.hh
+++ seastar/src/scheduling.hh
@@ -6,5 +6,9 @@
 namespace seastar_ffi {
 namespace scheduling {
 
+using scheduling_group = seastar::scheduling_group;
+
+std::unique_ptr<scheduling_group> new_sg();
+
 } // namespace scheduling
 } // namespace seastar_ffi
--- seastar/src/scheduling.rs
+++ seastar/src/scheduling.rs
@@ -1,8 +1,29 @@
+use cxx::UniquePtr;
+
 #[cxx::bridge(namespace = "seastar_ffi::scheduling")]
 mod ffi {
     unsafe extern "C++" {
         include!("seastar/src/scheduling.hh");
+
+        type scheduling_group;
+
+        fn new_sg() -> UniquePtr<scheduling_group>;
     }
 }
 
 use ffi::*;
+
+/// Identifies function calls that are accounted as a group.
+///
+/// A `SchedulingGroup` is a tag that can be used to mark a function call.
+/// Executions of such tagged calls are accounted as a group.
+pub struct SchedulingGroup {
+    pub(crate) inner: UniquePtr<scheduling_group>,
+}
+
+impl Default for SchedulingGroup {
+    /// Creates a `SchedulingGroup` instance denoting the default group.
+    fn default() -> Self {
+        SchedulingGroup { inner: new_sg() }
+    }
+}
