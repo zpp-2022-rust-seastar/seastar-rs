c0f2332127d29e27d2b5e002e22274ffcbcf12b7
Patryk Jędrzejczak <patryk.jed123@gmail.com>
2023-02-21 16:12:55 +0100

    Add SchedulingGroup::{create, destroy, rename}
    This commit introduces 3 methods/functions of `SchedulingGroup` that
    correspond to friend functions of `seastar::scheguduling_group` class:
    `create`, `destroy`, `rename`. We do this this way because there is such
    concept as friend funcions in Rust. Besides, this approach seems to have
    no disadvantages.
    
    Implementing `create` is quite tricky because we can't await a future
    that waits for `scheduling_group` (or `UniquePtr<scheduling_group>`)
    in Rust. The workaround here is creating `UniquePtr<scheduling_group>`
    in Rust, passing its reference to `create_sg` and changing it to new
    `UniquePtr` with the value returned by `seastar::create_scheduling_group`.

--- seastar/src/scheduling.cc
+++ seastar/src/scheduling.cc
@@ -28,5 +28,20 @@ bool sg_equal(const scheduling_group& sg1, const scheduling_group& sg2) {
     return sg1 == sg2;
 }
 
+VoidFuture create_sg(std::unique_ptr<scheduling_group>& sg, rust::str name, float shares) {
+    seastar::sstring s_name(name.begin(), name.size());
+    scheduling_group new_sg = co_await seastar::create_scheduling_group(s_name, shares);
+    sg = std::make_unique<scheduling_group>(std::move(new_sg));
+}
+
+VoidFuture destroy_sg(const std::unique_ptr<scheduling_group>& sg) {
+    co_await seastar::destroy_scheduling_group(*sg);
+}
+
+VoidFuture rename_sg(const std::unique_ptr<scheduling_group>& sg, rust::str new_name) {
+    seastar::sstring s_name(new_name.begin(), new_name.size());
+    co_await seastar::rename_scheduling_group(*sg, s_name);
+}
+
 } // namespace scheduling
 } // namespace seastar_ffi
