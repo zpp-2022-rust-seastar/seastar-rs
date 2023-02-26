beb53ea9e01903b8022057e1b8042fa6a60fa5aa
Patryk Jędrzejczak <patryk.jed123@gmail.com>
2023-02-26 14:51:33 +0100

    Set up callback passing across ffi
    This commit does everything that is needed to pass callbacks implementing
    the `FnMut()` trait across ffi.
    
    Apart from the callback we pass to C++:
    - a caller – created by `get_caller` function, calls the callback,
    - a dropper – created by `get_dropper` function, drops the callback.
    
    To pass the callback to C++ we will put it inside a `Box` and cast it to
    `*mut u8` − a substitue for `*mut c_void` that is not supported by `cxx`.
    
    We want to drop the callback in a right moment. That's why we create
    the `callback_object` class which is a callable object that drops the
    callback when its destructor is called. We overwrite move constructor and
    move operator of the `callback_object` to avoid droppping the callback
    in a temporary instance's destructor. We will pass instances of this class
    instead of pure callbacks to timers' `set_callback` methods.

--- seastar/src/timer.cc
+++ seastar/src/timer.cc
@@ -3,6 +3,57 @@
 namespace seastar_ffi {
 namespace timer {
 
+// This class was created to drop callbacks passed to `timer::set_calback` at the right
+// time. We pass an instance of` callback_object` instead of a pure callback and the
+// callback is dropped when the `callback_object`'s destructor is called.
+class callback_object {
+    uint8_t* _callback;
+    rust::Fn<void(uint8_t*)> _caller;
+    rust::Fn<void(uint8_t*)> _dropper;
+    bool _valid = true;
+
+public:
+    callback_object(
+        uint8_t* callback,
+        rust::Fn<void(uint8_t*)> caller,
+        rust::Fn<void(uint8_t*)> dropper
+    ) : _callback(callback), _caller(caller), _dropper(dropper) {}
+
+    callback_object(const callback_object&) = delete;
+
+    callback_object& operator=(const callback_object&) = delete;
+
+    callback_object(
+        callback_object&& other
+    ) : _callback(other._callback), _caller(other._caller), _dropper(other._dropper) {
+        other._valid = false;
+    }
+
+    callback_object& operator=(callback_object&& other) {
+        if (this != &other) {
+            _callback = other._callback;
+            _caller = other._caller;
+            _dropper = other._dropper;
+            other._valid = false;
+        }
+
+        return *this;
+    }
+
+    // We want to drop `_callback` only if an instance of `callback_object` wasn't move.
+    // Without this, the destructor of a temporary instance created in `sct_set_callback`
+    // would drop `_callback` too early and there would be a double drop.
+    ~callback_object() {
+        if (_valid) {
+            _dropper(_callback);
+        }
+    }
+
+    void operator()() {
+        _caller(_callback);
+    }
+};
+
 namespace steady_clock {
 
 } // namespace steady_clock
--- seastar/src/timer.rs
+++ seastar/src/timer.rs
@@ -1,1 +1,1 @@
-
+use crate::ffi_utils::{get_dropper, get_fn_mut_void_caller};
